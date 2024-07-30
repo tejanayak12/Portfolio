1c4ea90c2e6e567b3ee7af56bb998ef61615523c
Teja Nayak <prasadnaik1092@gmail.com>
2024-07-30 21:32:27 +0530

    firstcommit

---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            gridTemplateColumns: {
+                'auto': 'repeat(auto-fit,minmax(200px, 1fr))'
+            },
+            fontFamily: {
+                Outfit: ["Outfit", "sans-serif"],
+                Ovo: ["Ovo", "serif"]
+            },
+            animation: {
+                spin_slow: 'spin 6s linear infinite'
+            },
+            colors: {
+                lightHover: '#fcf4ff',
+                darkHover: '#2a004a',
+                darkTheme: '#11001f'
+            },
+            boxShadow: {
+                'black': '4px 4px 0 #000',
+                'white': '4px 4px 0 #fff'
+            }
+        }
+
+    },
+
+    darkMode: 'selector'
+
+}
+
+// tailwind.config.js
+module.exports = {
+    content: ['./src/**/*.{html,js}'], // Adjust according to your file structure
+    theme: {
+        extend: {
+            backdropBlur: {
+                lg: '10px', // Adjust the blur size as needed
+            },
+        },
+    },
+    plugins: [],
+};
